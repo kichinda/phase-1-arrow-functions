6fa421b510fa090003c7e9577a384d420a42553d
kichinda <kichindajesse@gmail.com>
2022-11-28 15:50:56 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a,b) {
+  return a/b;
+}
+console.log((2000, 100));
+divide()
+
+const square=(a)=>a**a
+console.log(square(10))
+square();
+
+let add=(a,b)=>a+b
+console.log(add(3,4));
+add();
